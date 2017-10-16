301aa56a6caa07a4b1dd2baff9d593315d951688
ian-ramos <ramos.ianc@gmail.com>
2017-10-16 16:08:16 -0400

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,38 @@
-function getFahrenheits(result){
-  // Your code goes here
+function getFahrenheits(result) {
+  // figured out result by putting debugger and refreshing page.  Debugger hits b/c this is run on load by init.js
+  return result.hourly_forecast.map(hour => hour.temp.english);
 }
 
-function getHours(result){
+function getHours(result) {
   // Your code goes here
+  return result.hourly_forecast.map(x => x.FCTTIME.hour);
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  // figured out return value by looking at test
+  return Object.assign(
+    {},
+    { labels: labels },
+    {
+      datasets: [
+        {
+          label: "Hourly Weather for New York",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: data
+        }
+      ]
+    }
+  );
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  fetch(endpoint)
+    .then(res => res.json())
+    .then(json => success(json));
 }
